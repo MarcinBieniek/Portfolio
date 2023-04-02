2b5b9e7b2472a1881c97556a073477fb6a37aa16
Marcin Bieniek <marcingim@gmail.com>
2023-04-02 14:03:22 +0200

    Add Contact component with form

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,9 +1,100 @@
 import styles from './Contact.module.scss';
+import EmailIcon from '@mui/icons-material/Email';
+import { useEffect, useState } from 'react';
+import { useForm } from '@formspree/react';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 
 const Contact = () => {
+
+  const [state, handleSubmit] = useForm("mjvdaagk");
+  const [send, setSend] = useState(false)
+
+  useEffect(() => {
+    setSend(true)
+    setTimeout(() => {
+      setSend(false)
+         }, 3000);
+  }, [state.submitting])
+
+  console.log('state is', state)
+  console.log('send is', send)
+
   return (
-    <div className={styles.container} id="contact">
-      asd
+    <div className={styles.contact} id="contact">
+      <div className={styles.header}>
+        <img
+          className={styles.icon}
+          src={`${process.env.PUBLIC_URL}/images/sun.svg`} 
+          alt="Main image"
+        />
+        <h1>Get in touch</h1>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <div className={styles.left_wrapper}>
+            <div className={styles.contact_box}>
+              <EmailIcon className={styles.icon}/>
+              <div>
+                <h1>Message Me</h1>
+                <span>hello@marcindev.com</span>
+              </div>
+            </div>
+            <div className={styles.contact_box}>
+              <EmailIcon className={styles.icon}/>
+              <div>
+                <h1>Let's talk</h1>
+                <span>(+48) 694 943 167</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right}>
+
+          {send
+          
+          ? 
+
+          <div className={styles.success}>
+            <HandshakeIcon className={styles.icon}/>
+            <h1>Message sent!</h1>
+            <span>Thank You for reaching out!</span>
+          </div>
+
+          :
+
+          <form onSubmit={handleSubmit}>
+            <div className={styles.form_field}>
+              <label htmlFor="email">
+                Email Address
+              </label>
+              <input
+                id="email"
+                type="email" 
+                name="email"
+              />
+            </div>
+            <div className={styles.form_field}>
+              <label htmlFor="email">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+              />
+            </div>
+            <button type="submit" disabled={state.submitting}>
+              Send              
+            </button>
+          </form>
+
+          } 
+          
+        </div>
+          
+      </div>
+
+      
+
     </div>
   )
 }
